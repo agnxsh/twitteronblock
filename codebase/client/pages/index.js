d3b43a9a3301fd7d0c90efd0847ea728a6f3d719
agnxsh <iamagnix@gmail.com>
2022-03-05 21:43:52 +0530

    first commit

---
+++ codebase/client/pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+const style = {
+  wrapper:
+    'flex justify-center h-screen w-screen select-none bg-[#15202b] text-white',
+  content: 'max-w-[1400px] w-2/3 flex justify-between',
+}
+
+export default function Home() {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.content}>
+        <Sidebar />
+        <h2>Feed</h2>
+        <h2>Widgets</h2>
+      </div>
+    </div>
+  )
+}
