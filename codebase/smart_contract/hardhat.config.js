ebdf15ed4aefb0c4c50433181c75e650401626aa
agnxsh <iamagnix@gmail.com>
2022-03-17 03:43:54 +0530

    done :D

--- codebase/smart_contract/hardhat.config.js
+++ codebase/smart_contract/hardhat.config.js
@@ -1,11 +1,9 @@
-require("@nomiclabs/hardhat-waffle");
+import "@nomiclabs/hardhat-waffle";
 
-module.exports = {
-  solidity: "0.8.2",
-  networks: {
-    rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/vD-ec9n03lUMfDAm6dlgLsnTZThJrMkX",
-      accounts: process.env.METAMASK_PRIVATE_KEY,
-    },
+export const solidity = "0.8.2";
+export const networks = {
+  rinkeby: {
+    url: "https://eth-rinkeby.alchemyapi.io/v2/vD-ec9n03lUMfDAm6dlgLsnTZThJrMkX",
+    accounts: process.env.METAMASK_PRIVATE_KEY,
   },
 };
